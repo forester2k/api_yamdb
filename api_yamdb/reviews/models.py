4349eee5072cc948c1217995df67828d136dbd85
Кирилл Васильев <kik2000@yandex.ru>
2022-05-08 11:07:10 +0300

    update ReadOnlyTitleSerializer and Title

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 
+from .validators import validate_year
+
 User = get_user_model()
 
 
@@ -35,7 +37,8 @@ class Title(models.Model):
         verbose_name='Название',
         max_length=200
     )
-    year = models.IntegerField(
+    year = models.PositiveSmallIntegerField(
+        validators=[validate_year],
         verbose_name='Год',
     )
     genre = models.ManyToManyField(
@@ -55,13 +58,14 @@ class Title(models.Model):
         null=True
     )
 
-    def __str__(self):
-        return f'{self.name}'
-
     class Meta:
         verbose_name = 'Произведение'
         verbose_name_plural = 'Произведения'
         ordering = ['name']
+
+
+    def __str__(self):
+        return f'{self.name}'
 
 
 class GenreTitle(models.Model):
